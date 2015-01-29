b1d9cb5f0e6ccd15bd96e928540a10246dd45318
Diego Avalon <diegoavalon@live.com>
2015-01-29 14:42:47 -0800

    finish styling and interactivity of category sizing

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,99 @@
 $(function()	{
 	$("#results-section").hide();
-	$(".btn-black").click(function() {
-		$("#sizing-section").hide();
-		$("#results-section").show().delay(5000);
+	$("#loader").hide();
 
+	$("#sizingSocks").submit(function(event) {
+		$("#sizing-section").hide();
+		$("#loader").show();
+		$("#loader").delay(5000).fadeOut();
+
+		// var shoeSizeInput = $("select#shoeSize").val();
+		// var gender = $("input[name='optionsGender']:checked").val();
+		
+
+		$("#results-section").delay(5500).fadeIn(1000);
+
+
+		$(".size").append("medium append");
+
+		event.preventDefault();
+		
 	});
+
+
+	$("#sizingGloves").submit(function(event) {
+		$("#sizing-section").hide();
+		$("#loader").show();
+		$("#loader").delay(5000).fadeOut();
+
+		// var shoeSizeInput = $("select#shoeSize").val();
+		// var gender = $("input[name='optionsGender']:checked").val();
+		
+
+		$("#results-section").delay(5500).fadeIn(1000);
+
+
+		$(".size").append("medium append");
+
+		event.preventDefault();
+		
+	});
+
+
+	$("#sizingBelts").submit(function(event) {
+		$("#sizing-section").hide();
+		$("#loader").show();
+		$("#loader").delay(5000).fadeOut();
+
+		// var shoeSizeInput = $("select#shoeSize").val();
+		// var gender = $("input[name='optionsGender']:checked").val();
+		
+
+		$("#results-section").delay(5500).fadeIn(1000);
+
+
+		$(".size").append("medium append");
+
+		event.preventDefault();
+		
+	});
+
+
+	$("#sizingBreeches").submit(function(event) {
+		$("#sizing-section").hide();
+		$("#loader").show();
+		$("#loader").delay(5000).fadeOut();
+
+		// var shoeSizeInput = $("select#shoeSize").val();
+		// var gender = $("input[name='optionsGender']:checked").val();
+		
+
+		$("#results-section").delay(5500).fadeIn(1000);
+
+
+		$(".size").append("medium append");
+
+		event.preventDefault();
+		
+	});
+
+
+	$("#sizingTops").submit(function(event) {
+		$("#sizing-section").hide();
+		$("#loader").show();
+		$("#loader").delay(5000).fadeOut();
+
+		// var shoeSizeInput = $("select#shoeSize").val();
+		// var gender = $("input[name='optionsGender']:checked").val();
+		
+
+		$("#results-section").delay(5500).fadeIn(1000);
+
+
+		$(".size").append("medium append");
+
+		event.preventDefault();
+		
+	});
+
 });
